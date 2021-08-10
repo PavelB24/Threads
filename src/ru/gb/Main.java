f8c521ee7512941969ae58ef1288e785bbc594d2
Pavel <alkatrazzzzz@mail.ru>
2021-08-10 20:33:01 +0300

    v0.2

--- src/ru/gb/Main.java
+++ src/ru/gb/Main.java
@@ -19,24 +19,33 @@ public class Main {
         tr1.start();
 
         try {
-            testTwoThreads(tr1, arr1);
+            testTwoThreads();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         testOneThread();
     }
 
-    private static void testTwoThreads(Thread tr1, float[] arr1) throws InterruptedException {
+    private static void testTwoThreads() throws InterruptedException {
+        float[] arr1 = new float[HALF];
         float[] arr2 = new float[HALF];
+        Arrays.fill(arr, 1);
         long a = System.currentTimeMillis();
-        tr1.join();
+        Thread tr1 = new Thread(() -> {
+
+            System.arraycopy(arr, 0, arr1, 0, HALF);
+            for (int i = 0; i < arr1.length; i++) {
+                arr1[i] = (float) (arr1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+        tr1.start();
         System.arraycopy(arr, HALF, arr2, 0, HALF);
 
         for (int i = 0; i < arr2.length; i++) {
             arr[i] = (float) (arr[i] * Math.sin(0.2f + (i + HALF) / 5) * Math.cos(0.2f + (i + HALF) / 5) * Math.cos(0.4f + i + (HALF) / 2));
         }
 
-        System.out.println();
+        tr1.join();
         System.arraycopy(arr1, 0, arr, 0, arr1.length);
         System.arraycopy(arr2, 0, arr, HALF, arr1.length);
 
